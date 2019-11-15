e2552359aaf82c80b5b279a8d79ab6a70a191acb
Kamikaze2101 <57804028+Kamikaze2101@users.noreply.github.com>
2019-11-15 17:07:51 +0200

    Add files via upload

---
+++ new/js/image2.js
@@ -0,0 +1,36 @@
+setTimeout(function() {
+	m3D.init(
+		[ 
+			{ src: '8.1.jpg', url: 'http://www.htmldrive.net', title: 'jump to random script', color: '#fff' },
+			{ src: '9.1.jpg' },
+			{ src: '10.1.jpg' },
+			{ src: '11.1.jpg' },
+			{ src: '12.1.jpg' },
+			{ src: '13.1.jpg' },
+			{ src: '6a884d6ef2.jpg' },
+			{ src: 'ed57bce8c4.jpg' },
+			{ src: '94da4fb185.jpg' },
+			{ src: 'd04505fe96.jpg' },
+			{ src: '46eae50db6.jpg', url: 'http://www.htmldrive.net', title: 'jump to random script', color: '#fff' },
+			{ src: '1ef7ea6559.jpg' },
+			{ src: '8fe86226ff.jpg' },
+			{ src: '69833ed09a.jpg' },
+			{ src: 'fe00425d0a.jpg' },
+			{ src: 'b05b0c6962.jpg' },
+			{ src: '6a884d6ef2.jpg' },
+			{ src: 'ed57bce8c4.jpg' },
+			{ src: '94da4fb185.jpg' },
+			{ src: 'd04505fe96.jpg' },
+			{ src: '46eae50db6.jpg', url: 'http://www.htmldrive.net', title: 'jump to random script', color: '#fff' },
+			{ src: '1ef7ea6559.jpg' },
+			{ src: '8fe86226ff.jpg' },
+			{ src: '69833ed09a.jpg' },
+			{ src: 'fe00425d0a.jpg' },
+			{ src: 'b05b0c6962.jpg' },
+			{ src: '6a884d6ef2.jpg' },
+			{ src: 'ed57bce8c4.jpg' },
+			{ src: '94da4fb185.jpg' },
+			{ src: 'd04505fe96.jpg' },
+		]
+	);
+}, 500);
